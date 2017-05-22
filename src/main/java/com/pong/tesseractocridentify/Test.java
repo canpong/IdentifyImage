72be05ea45de739eaab6bc4207b56e11a1d56ec4
canpong <wjp20130625@163.com>
2017-05-22 22:46:35 +0800

    add project

---
+++ src/main/java/com/pong/tesseractocridentify/Test.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.pong.tesseractocridentify;
+
+import java.io.File;
+
+/**
+ * @Description
+ * @author canpong wu
+ * @CreateTime 2017年5月22日 下午2:43:54
+ * @version
+ */
+public class Test {
+    public static void main(String[] args)  
+    {  
+        try  
+        {  
+              
+            File testDataDir = new File("D:/Program Files/Tesseract-OCR/file");  
+//            System.out.println(testDataDir.listFiles().length);  
+            int i = 0 ;   
+            for(File file :testDataDir.listFiles())  
+            {  
+                i++ ;  
+                String recognizeText = new OCRHelper().recognizeText(file);  
+                System.out.println();
+                System.out.print(recognizeText+"\t");  
+  
+                if( i % 5  == 0 )  
+                {  
+                    System.out.println();  
+                }  
+            }  
+              
+        } catch (Exception e)  
+        {  
+            e.printStackTrace();  
+        } 
+    }
+}
